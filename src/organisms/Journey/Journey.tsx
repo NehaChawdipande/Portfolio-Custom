ae004755a1c5ffbe748d59d49e82a0a3fdc80c8a
neha.chawdipande <neha.chawdipande@specsavers.com>
2024-03-01 13:52:45 +0530

    Updated the styles and fixed some changes

--- src/organisms/Journey/Journey.tsx
+++ src/organisms/Journey/Journey.tsx
@@ -9,22 +9,27 @@ const Journey = (): JSX.Element => {
        <div className="responsiveImg2">
         <img id="dev" src="/journey.png" alt="dev" />
       </div>
-      <div className="text" style={{paddingLeft:"0", paddingRight: "10%"}} >
+      <div className="text" >
         <h1 id="hi" style={{ marginBottom: "0"}}>Journey</h1>
-        <h3 style={{ marginTop: "0"}}>Still a long way to go..</h3>
+        <h3 style={{ marginTop: "0", color:"#7a887b"}}>Still a long way to go..</h3>
         <div className="markerLeft">
             <IconButton
                 aria-label="checkpoint1">
                 <GolfCourseIcon className="markerIcon" />
               </IconButton>
-              <h4>Custom Software Development Analyst @Accenture | Sept 2024 - Preset</h4>
+              <h4>Custom Software Development Analyst @Accenture
+              <br/>
+              <strong style={{color:"#7a887b"}}>Feb 2023 - Present</strong>
+              </h4>
         </div>
         <div className="markerLeft">
             <IconButton
                 aria-label="checkpoint1">
                 <GolfCourseIcon className="markerIcon" />
               </IconButton>
-              <h4>Application Development Analyst  @Accenture | Sept 2022 - Jan 2023</h4>
+              <h4>Application Development Analyst @Accenture <br/>
+             <strong style={{color:"#7a887b"}}> Sep 2022 - Jan 2023 </strong>
+              </h4>
         </div>
 
         <div className="markerLeft">
@@ -32,7 +37,10 @@ const Journey = (): JSX.Element => {
                 aria-label="checkpoint1">
                 <GolfCourseIcon className="markerIcon" />
               </IconButton>
-              <h4>Application Development Associate @Accenture| June 2021 - July 2022</h4>
+              <h4>Application Development Associate @Accenture
+                <br/>
+                <strong style={{color:"#7a887b"}}> Jun 2021 - Jul 2022 </strong>
+              </h4>
         </div>
         
         <div className="markerLeft">
@@ -40,7 +48,10 @@ const Journey = (): JSX.Element => {
                 aria-label="checkpoint1">
                 <GolfCourseIcon className="markerIcon" />
               </IconButton>
-              <h4>Intern @TechMahindra| March 2021 - May 2021</h4>
+              <h4>Intern @TechMahindra
+                <br />
+                <strong style={{color:"#7a887b"}}> March 2021 - May 2021</strong>
+              </h4>
         </div>
       </div>
     </div>
