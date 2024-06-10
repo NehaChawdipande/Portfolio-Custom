7e658509e213c42d0bb24e73424bb6556ec66a1c
NehaChawdipande <nehachawdipande10@gmail.com>
2024-06-11 01:07:17 +0530

    updated projects

--- src/organisms/Contact/Contact.tsx
+++ src/organisms/Contact/Contact.tsx
@@ -5,9 +5,9 @@ import ContactForm from "./ContactForm";
 const Contact = (): JSX.Element => {
   return (
 
-    <div className="page3" id="page4" style={{gridTemplateColumns: "45% 55%", minHeight: "80vh"}}>
+    <div className="page3" id="page4" style={{gridTemplateColumns: "45% 55%", minHeight: "75vh"}}>
          
-      <div className="responsiveImg2" style={{margin: "0 18px"}}>
+      <div className="responsiveImg2">
       
         <img id="dev" src="/15.png" alt="dev" style={{ marginTop: "0" }} />
       </div>
